743acae4b9425d1b75141bd55e69ccc71698db9f
Frederick <fred.chien3@gmail.com>
2022-08-26 14:03:51 -0400

    create Cat model validations

--- ninety-nine-cats/app/models/cat.rb
+++ ninety-nine-cats/app/models/cat.rb
@@ -1,4 +1,4 @@
-# == Schema Information
+# == ScheMa InFormation
 #
 # Table name: cats
 #
@@ -11,9 +11,25 @@
 #  created_at  :datetime         not null
 #  updated_at  :datetime         not null
 #
+
+require 'action_view'
+
 class Cat < ApplicationRecord
+    include ActionView::Helpers::DateHelper
+
+    CAT_COLORS = ["red", "yellow", "blue"]
     validates_presence_of :birth_date, :color, :name, :sex
+    validates :color, inclusion: { in: CAT_COLORS}
+    validates :sex, inclusion: { in: ['M', 'F'] }
+    validate :birth_date_cannot_be_future
 
+    def birth_date_cannot_be_future
+        if birth_date.present? && birth_date > Date.today
+            errors.add(:birth_date, "can't be in the future")
+        end
+    end
 
-
+    def age
+       time_ago_in_words(birth_date)
+    end
 end
